9849f5d8567ac0b06f110929ca5722a986a2fd79
Amr Elshaer <AElshaer.c@tasheer.com>
2025-10-20 09:00:29 +0300

    try to call mcp server project

--- HelloWordAgents/Program.cs
+++ HelloWordAgents/Program.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Agents.AI.Workflows;
-using Microsoft.Extensions.AI;
-using System.ComponentModel;
-using Microsoft.Agents.AI;
+﻿using Microsoft.Extensions.AI;
+using ModelContextProtocol.Client;
 using OpenAI;
 using OpenAI.Chat;
 using System.ClientModel;
@@ -11,45 +9,47 @@ IChatClient chatClient =
             "gpt-4o-mini",
             new ApiKeyCredential(apiKey!),
             new OpenAIClientOptions { Endpoint = new Uri("https://models.github.ai/inference") })
-        .AsIChatClient();
-
-AIAgent writer = new ChatClientAgent(
-    chatClient,
-    new ChatClientAgentOptions
+  
+        .AsIChatClient()
+        .AsBuilder()
+        .UseFunctionInvocation()
+        .Build();
+var mcpClient = await McpClient.CreateAsync(
+    new StdioClientTransport(new()
     {
-        Name = "Writer",
-        Instructions = "Write stories that are engaging and creative.",
-        ChatOptions = new ChatOptions
-        {
-            Tools = [
-                AIFunctionFactory.Create(GetAuthor),
-                AIFunctionFactory.Create(FormatStory)
-            ],
-        }
-    });
-AIAgent editor = new ChatClientAgent(
-    chatClient,
-    new ChatClientAgentOptions
+     
+        Command = "dotnet run",
+        Arguments = ["--project", "C:\\projects\\personal_projects\\McpServerProject\\McpServerProject\\McpServerProject.csproj"],
+        Name = "Mcp server sample",
+        EnvironmentVariables = new Dictionary<string, string>
     {
-        Name = "Editor",
-        Instructions = "Make the story more engaging, fix grammar, and enhance the plot."
-    });
+        { "WEATHER_CHOICES", "sunny,rainy,cloudy,snowy,windy" }
+    }
+    }));
+// List all available tools from the MCP server.
+Console.WriteLine("Available tools:");
+IList<McpClientTool> tools = await mcpClient.ListToolsAsync();
+foreach (McpClientTool tool in tools)
+{
+    Console.WriteLine($"{tool}");
+}
+Console.WriteLine();
 
-// Create a workflow that connects writer to editor
-Workflow workflow =
-    AgentWorkflowBuilder
-        .BuildSequential([writer, editor]);
+// Conversational loop that can utilize the tools via prompts.
+List<Microsoft.Extensions.AI.ChatMessage> messages = [];
+while (true)
+{
+    Console.Write("Prompt: ");
+    messages.Add(new(ChatRole.User, Console.ReadLine()));
 
-AIAgent workflowAgent = await workflow.AsAgentAsync();
+    List<ChatResponseUpdate> updates = [];
+    await foreach (ChatResponseUpdate update in chatClient
+        .GetStreamingResponseAsync(messages, new() { Tools = [.. tools] }))
+    {
+        Console.Write(update);
+        updates.Add(update);
+    }
+    Console.WriteLine();
 
-AgentRunResponse workflowResponse =
-    await workflowAgent.RunAsync("Write a short story about a haunted house.");
-
-Console.WriteLine(workflowResponse.Text);
-
-[Description("Gets the author of the story.")]
-string GetAuthor() => "Jack Torrance";
-
-[Description("Formats the story for display.")]
-string FormatStory(string title, string author, string story) =>
-    $"Title: {title}\nAuthor: {author}\n\n{story}";
+    messages.AddMessages(updates);
+}
